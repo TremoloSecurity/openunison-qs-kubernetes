e779fb0995c4b3e8d9486b52abf05700c77b7b0e
mlbiam <marc.boorshtein@tremolosecurity.com>
2018-09-23 22:21:52 -0400

    removing debug messages

--- src/main/js/deploy.js
+++ src/main/js/deploy.js
@@ -57,9 +57,6 @@ csrReq = {
 print("Requesting certificate");
 apiResp = k8s.postWS('/apis/certificates.k8s.io/v1beta1/certificatesigningrequests',JSON.stringify(csrReq));
 
-print("request code : " + apiResp.code);
-print("request json : " + apiResp.data);
-
 print("Approving certificate");
 approveReq = JSON.parse(apiResp.data);
 approveReq.status.conditions = [
@@ -75,7 +72,6 @@ print("Retrieving certificate from API server");
 apiResp = k8s.callWS('/apis/certificates.k8s.io/v1beta1/certificatesigningrequests/openunison.openunison.svc.cluster.local');
 certResp = JSON.parse(apiResp.data);
 b64cert = certResp.status.certificate;
-print(b64cert);
 CertUtils.importSignedCert(x509data,b64cert);
 
 print("Saving certificate to keystore");
